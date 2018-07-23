fe9569a5aa2796cad129dd563573890a4a655424
Nilesh Patkar <nilesh.patkar@mpllc.com>
2018-07-24 04:47:50 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  
+  styleUrls: ['./app.component.css'],
+  template : `
+    <button class="btn btn-primary" [class.active] = "isActive">Save</button>
+    <br/>
+    <div (click)="onDivClicked()">
+      <button (click)="OnSave($event)">Save Click</button>
+    </div>  
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular App';
+  isActive = false;
+
+  onDivClicked(){
+    console.log("Div was clicked");
+  }
+
+  OnSave($event){
+    $event.stopPropagation();
+    console.log("On Save is clicked", $event);
+  }
 }
